0e8fb30bc61534aef370516db74fc9c44cb7289e
David Pashaev <pashdav@gmail.com>
2020-05-19 00:11:14 +0700

    Fixes error on adding payment gateway

---
+++ tests/src/FunctionalJavascript/YandexCheckoutPaymentGatewayTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\yandex_checkout\FunctionalJavascript;
+
+/**
+ * Tests the admin UI for yandex_checkout payment gateway.
+ *
+ * @group commerce_yandex_checkout
+ */
+class YandexCheckoutPaymentGatewayTest extends PaymentGatewayTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $pluginId = 'yandex_checkout';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getPluginConfiguration() {
+    return [
+        'shop_id' => 87654321,
+        'secret_key' => 'test_3iEwkXIv5RmV8quKUPadlMIK8oSomIbA6hiU23mSxwU',
+        'display_label' => 'Yandex Checkout',
+        'description_template' => 'Order #%order_id%',
+        'receipt_enabled' => 1,
+    ];
+  }
+
+}
